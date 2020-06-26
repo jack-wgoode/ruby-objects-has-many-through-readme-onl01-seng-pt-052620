e9f6989f9669dc13d910220ec094f5a6b6cb0177
Jack Goode <jack.w.goode@icloud.com>
2020-06-26 01:00:12 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,5 @@
 class Customer
-   attr_accessor :name, :age
+  attr_accessor :name, :age
  
   @@all = []
  
@@ -10,7 +10,40 @@ class Customer
   end
  
   def self.all
-    @@all
+      @@all
+  end
+ 
+  def meals
+      Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+ 
+  def waiters
+      meals.map do |meal|
+      meal.waiter
+    end
+  end
+ 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+  def new_meal_20_percent(waiter, total)
+    tip = total * 0.2
+    Meal.new(waiter, self, total, tip)
+  end
+ 
+  def self.oldest_customer
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
   end
  
 end
